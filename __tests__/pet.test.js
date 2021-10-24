c6190442db8daa47c93b0636a5d6fc61e558d764
PemMmM <pembemistikoglu992@gmail.com>
2021-10-25 02:04:25 +0300

    not Alive

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -15,6 +15,8 @@ describe("constructor", () => {
 });
 
 describe("growUp", () => {
+
+    
   it("increments the age by 1", () => {
     const pet = new Pet("Fido");
     pet.growUp();
@@ -32,14 +34,23 @@ describe("growUp", () => {
     pet.growUp();
     expect(pet.fitness).toEqual(7);
   });
+
+  it('throws an error if the pet is not alive', () => {
+    const pet = new Pet('Fido');
+    pet.age = 30;
+    expect(() => pet.growUp()).toThrow('Your pet is no longer alive :(');
+  });
+
 });
 
 describe("walk", () => {
+
+
     it("increases fitness level by 4", () => {
         const pet = new Pet("Fido");
-        pet.fitness = 0;
+        pet.fitness = 5;
         pet.walk();
-        expect(pet.fitness).toEqual(4);
+        expect(pet.fitness).toEqual(9);
 
     });
 
@@ -49,9 +60,17 @@ describe("walk", () => {
         pet.walk();
         expect(pet.fitness).toEqual(10);
       });
+
+      it('throws an error if the pet is not alive', () => {
+        const pet = new Pet('Fido');
+        pet.age = 30;
+        expect(() => pet.walk()).toThrow('Your pet is no longer alive :(');
+      });
 });
 
 describe("feed", () => {
+
+
     it("decreases hunger level by 3", () => { 
         const pet = new Pet('fido');
         pet.hunger = 0;
@@ -75,10 +94,49 @@ describe("feed", () => {
         expect(pet.hunger).toEqual(0);
     
     })
-})
+
+    it('throws an error if the pet is not alive', () => {
+        const pet = new Pet('Fido');
+        pet.age = 30;
+        expect(() => pet.feed()).toThrow('Your pet is no longer alive :(');
+      });
+});
+
 
 describe("checkup", () => {
-    it()
+
+
+    it("should check if pet is hungry or fit", () => {
+        const pet = new Pet('fido');
+        pet.fitness = 2;
+        pet.hunger = 4;
+        pet.checkup();
+        expect(pet.checkup()).toEqual("I need a walk")
+
+    })
+
+    it("should check if pet is hungry or fit", () => {
+        const pet = new Pet('fido');
+        pet.fitness = 1;
+        pet.hunger = 7;
+        pet.checkup();
+        expect(pet.checkup()).toEqual("I am hungry AND I need a walk")
+
+    })
+    it("should check if pet is hungry or fit", () => {
+        const pet = new Pet('fido');
+        pet.fitness = 7;
+        pet.hunger = 2;
+        pet.checkup();
+        expect(pet.checkup()).toEqual("I feel great!")
+
+    })
+
+    it('throws an error if the pet is not alive', () => {
+        const pet = new Pet('Fido');
+        pet.age = 30;
+        expect(() => pet.checkup()).toThrow('Your pet is no longer alive :(');
+      });
 })
 
 
--- src/pet.js
+++ src/pet.js
@@ -5,16 +5,32 @@ function Pet(name) {
   this.fitness = 10;
 }
 
+
+
+Pet.prototype = {
+  get isAlive() {
+    return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+  },
+};
+
 const MAXIMUM_FITNESS = 10;
 const MIN_HUNGER = 0;
 
 Pet.prototype.growUp = function () {
+  if (!this.isAlive) {
+    throw new Error("Your pet is no longer alive :(");
+  }
   this.age += 1;
   this.hunger += 5;
   this.fitness -= 3;
 };
 
 Pet.prototype.walk = function () {
+
+    if (!this.isAlive) {
+        throw new Error("Your pet is no longer alive :(");
+      }
+ 
   if (this.fitness + 4 <= MAXIMUM_FITNESS) {
     this.fitness += 4;
   } else {
@@ -23,10 +39,28 @@ Pet.prototype.walk = function () {
 };
 
 Pet.prototype.feed = function () {
+  if (!this.isAlive) {
+    throw new Error("Your pet is no longer alive :(");
+  }
   if (this.hunger - 3 > 0) {
     this.hunger -= 3;
   } else {
     this.hunger = MIN_HUNGER;
+  }
+};
+
+Pet.prototype.checkup = function () {
+  if (!this.isAlive) {
+    throw new Error("Your pet is no longer alive :(");
+  }
+  if (this.fitness <= 3 && this.hunger >= 5) {
+    return "I am hungry AND I need a walk";
+  } else if (this.hunger >= 5) {
+    return "I am hungry";
+  } else if (this.fitness <= 3) {
+    return "I need a walk";
+  } else {
+    return "I feel great!";
   }
 };
 
